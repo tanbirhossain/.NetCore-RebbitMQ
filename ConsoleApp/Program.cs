dee59a77eca32e00799f2146ab332f2cac50ec21
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-06-05 16:40:36 +0900

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        //sender
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello this is the sender application!");
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "msgKey",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    Console.WriteLine("Enter message to send");
+                    var msg = Console.ReadLine();
+                    var body = Encoding.UTF8.GetBytes(msg);
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "msgKey",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ ConsoleAppTwo/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsoleAppTwo
+{
+    class Program
+    {
+        /// <summary>
+        /// reciver application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello this is the reciver application!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "msgKey",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Recived {0}", message);
+                };
+                channel.BasicConsume(queue: "msgKey",
+                                    autoAck: true,
+                                    consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
